4a5b87fbe0351188821836b2761a979326ea2ee8
karimalik <me@karimdev.network>
2023-11-24 07:29:26 +0100

    fix database error
    Co-authored-by: xdcondor1 <karimkompissi@outlook.fr

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,10 @@
 import { Sequelize } from 'sequelize';
+import { env } from 'process';
 
 export const sequelize = new Sequelize({
     dialect: 'mysql',
     host: process.env.HOST,
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    username: 'root',
+    password: '',
+    database: 'task-manager',
 });

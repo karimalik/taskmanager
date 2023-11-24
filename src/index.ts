ba97d217740ab163062e1147d750c10a2a0f6a39
karimalik <me@karimdev.network>
2023-11-24 06:16:09 +0100

    create configured database
    Co-authored-by: xdcondor1 <karimkompissi@outlook.fr>

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from 'express';
 import { env } from 'process';
+import { sequelize } from './config/db';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+//symchronize model with database
+sequelize.sync().then(() => {
+    console.log('Database synced!');
+});
+
 app.use(express.json());
+
+//default route
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Hello world' });
 });

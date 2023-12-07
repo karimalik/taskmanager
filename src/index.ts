4127ae31e178a4d59e954a32d3f482a634ec2720
karimalik <me@karimdev.network>
2023-12-07 07:30:06 +0100

    add swagger package
    Co-authored-by: xdcondor1 <karimkompissi@outlook.fr>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import { sequelize } from './config/db';
 import taskRoutes from './routes/taskRoutes';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,6 +13,23 @@ sequelize.sync().then(() => {
 });
 
 app.use(express.json());
+
+// Swagger configuration
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Your API Title',
+            version: '1.0.0',
+            description: 'Your API Description',
+        },
+    },
+    apis: ['./routes/*.ts'], // Specify the path to your route files
+};
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Use task routes
 app.use('tasks', taskRoutes);
 
 //default route
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -3,6 +3,16 @@ import * as taskController from '../controllers/taskController';
 
 const router = Router();
 
+/**
+ * @swagger
+ * /tasks:
+ *   get:
+ *     summary: Retrieve a list of tasks
+ *     responses:
+ *       200:
+ *         description: Successful response
+ */
+
 router.get('/', taskController.getTasks);
 router.post('/', taskController.createTask);
 router.put('/:id', taskController.updateTask);
